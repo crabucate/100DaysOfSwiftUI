83f84c0a45ba5489c5dba8b98d433482c284b317
crabucate <info@crabucate.de>
2021-01-01 16:23:34 +0100

    Add Project 2 Flags

---
+++ Flags/Flags/FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsApp.swift
+//  Flags
+//
+//  Created by Felix Leitenberger on 01.01.21.
+//
+
+import SwiftUI
+
+@main
+struct FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
