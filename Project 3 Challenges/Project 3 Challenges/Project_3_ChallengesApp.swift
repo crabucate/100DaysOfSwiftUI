f771d417e6945bbbc8d39e9de0326451de90a189
crabucate <info@crabucate.de>
2021-01-03 10:12:58 +0100

    Challenges Day 24 affecting different projects

---
+++ Project 3 Challenges/Project 3 Challenges/Project_3_ChallengesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_3_ChallengesApp.swift
+//  Project 3 Challenges
+//
+//  Created by Felix Leitenberger on 03.01.21.
+//
+
+import SwiftUI
+
+@main
+struct Project_3_ChallengesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
