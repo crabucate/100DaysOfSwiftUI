59dc4f8290c5c143fbde6fee090bb760fdf59812
crabucate <info@crabucate.de>
2021-01-06 18:30:12 +0100

    Challenges solved

---
+++ 07-Word Scramble (Project 5)/README.md
@@ -0,0 +1,5 @@
+# Word Scramble (Day 29-31)
+
+A word game.
+
+![](images/wordScrambleChallenges.gif)
---
+++ 07-Word Scramble (Project 5)/Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Felix Leitenberger on 06.01.21.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
