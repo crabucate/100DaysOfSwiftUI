cbbef08db7d51fa689a14ae625ffb164169d0ad5
crabucate <info@crabucate.de>
2021-01-11 20:07:21 +0100

    Animation Project

---
+++ 08-Animations (Project 6)/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Felix Leitenberger on 07.01.21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 08-Animations (Project 6)/README.md
@@ -0,0 +1,7 @@
+# Animations
+
+This is not a real project. You can see a custom transition here.
+
+![](images/animations.gif)
+
+The challenges are all in Project 2 (Flags).
