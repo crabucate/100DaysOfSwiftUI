44069656668f8f57ea2db3403425ebf5db4de9fb
crabucate <info@crabucate.de>
2021-01-13 10:50:59 +0100

    Some nice effects for Flags Game

--- 03-Flags (Project 2)/Flags/ContentView.swift
+++ 03-Flags (Project 2)/Flags/ContentView.swift
@@ -7,6 +7,23 @@
 
 import SwiftUI
 
+
+// Day 24: Go back to project 2 and create a Flag-View that renders one flag image using the specific set of modifiers we had.
+
+struct Flag: View {
+    
+    let country: String
+    
+    var body: some View {
+        Image(country)
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+            .shadow(color: .black, radius: 2)
+    }
+}
+
+
 struct ContentView: View {
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
@@ -17,6 +34,10 @@ struct ContentView: View {
     @State private var scoreMessage = ""
     
     @State private var score = 0
+    @State private var rotationAngles = [0.0, 0.0, 0.0]
+    @State private var opacities = [1.0, 1.0, 1.0]
+    @State private var shakes = [0, 0 ,0]
+    
     
     var body: some View {
         ZStack {
@@ -38,7 +59,11 @@ struct ContentView: View {
                     Button(action: {
                         self.flagTapped(number)
                     }) {
-                        FlagImage(country: self.countries[number])
+                        Flag(country: self.countries[number])
+                            .rotation3DEffect(Angle(degrees: rotationAngles[number]), axis: (x: 0, y: 1, z: 0))
+                            .opacity(opacities[number])
+                            .animation(.default)
+                            .modifier(ShakeEffect(shakes: shakes[number]))
                     }
                 }
                 
@@ -58,37 +83,52 @@ struct ContentView: View {
     }
     
     
-    // Day 24: Go back to project 2 and create a FlagImage() view that renders one flag image using the specific set of modifiers we had.
-    
-    struct FlagImage: View {
-        
-        let country: String
-        
-        var body: some View {
-            Image(country)
-                .renderingMode(.original)
-                .clipShape(Capsule())
-                .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-                .shadow(color: .black, radius: 2)
-        }
-    }
-    
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
+            rotationAngles[number] += 360
+            opacities = [0.25, 0.25, 0.25]
+            opacities[number] = 1.0
             scoreTitle = "Correct"
             score += 1
             scoreMessage = "Your score is \(score)."
         } else {
+            withAnimation(.linear) {
+                shakes[number] = 3
+            }
             scoreTitle = "Wrong"
             scoreMessage = "This is the flag of \(countries[number])"
         }
         
-        showingScore = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+            showingScore = true
+        })
+        
+        
     }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        opacities = [1.0, 1.0, 1.0]
+        shakes = [0, 0, 0]
+    }
+}
+
+
+struct ShakeEffect: GeometryEffect {
+    
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        return ProjectionTransform(CGAffineTransform(translationX: -30 * sin(position * 2 * .pi), y: 0))
+    }
+    
+    init(shakes: Int) {
+        position = CGFloat(shakes)
+    }
+    
+    var position: CGFloat
+    var animatableData: CGFloat {
+        get { position }
+        set { position = newValue }
     }
 }
 
