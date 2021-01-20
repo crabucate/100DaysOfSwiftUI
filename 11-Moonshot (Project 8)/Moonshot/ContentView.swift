0f9d7c63b3a941c7a4303868ff330f846462722b
crabucate <info@crabucate.de>
2021-01-20 09:04:16 +0100

    Add Moonshot App

--- 11-Moonshot (Project 8)/Moonshot/ContentView.swift
+++ 11-Moonshot (Project 8)/Moonshot/ContentView.swift
@@ -8,31 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
     var body: some View {
-                VStack {
-                    GeometryReader { geo in
-                            Image("Example")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: geo.size.width)
-                                .onAppear(perform: {
-                                    print(geo)
-                                })
-                        
-                    
-                    }
-                    ScrollView(.vertical) {
-                        VStack(spacing: 10) {
-                            ForEach(0..<100) {
-                                Text("Item \($0)")
-                                    .font(.title)
-                            }
-                        }
-                        .frame(maxWidth: .infinity)
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: MissionView(mission: mission, astronauts: self.astronauts)) {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 44, height: 44)
+
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
                     }
                 }
             }
+            .navigationBarTitle("Moonshot")
         }
+        }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
