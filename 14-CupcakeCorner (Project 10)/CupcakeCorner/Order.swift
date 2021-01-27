44237217516b2a30eb4c3eeac7ee3fae24674dc0
crabucate <info@crabucate.de>
2021-01-27 15:11:04 +0100

    Cupcakes add

---
+++ 14-CupcakeCorner (Project 10)/CupcakeCorner/Order.swift
@@ -0,0 +1,8 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Felix Leitenberger on 27.01.21.
+//
+
+import Foundation
---
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Felix Leitenberger on 27.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
