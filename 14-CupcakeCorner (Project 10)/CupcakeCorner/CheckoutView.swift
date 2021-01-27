5ace0df4c6d95180d80791aca5e314ff24efbc4f
crabucate <info@crabucate.de>
2021-01-27 15:11:24 +0100

    Cupcake github

--- 14-CupcakeCorner (Project 10)/CupcakeCorner/CheckoutView.swift
+++ 14-CupcakeCorner (Project 10)/CupcakeCorner/CheckoutView.swift
@@ -8,13 +8,77 @@
 import SwiftUI
 
 struct CheckoutView: View {
+    @ObservedObject var order: Order
+    
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
+    @State private var showingConnectionError = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            ScrollView {
+                VStack {
+                    Image("cupcakes")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width)
+
+                    Text("Your total is $\(self.order.cost, specifier: "%.2f")")
+                        .font(.title)
+
+                    Button("Place Order") {
+                        self.placeOrder()
+                    }
+                    .padding()
+                }
+            }
+        }
+        .navigationBarTitle("Check out", displayMode: .inline)
+        .alert(isPresented: $showingConfirmation) {
+            Alert(title: Text("Thank you!"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+        }
+        .alert(isPresented: $showingConnectionError, content: {
+            Alert(title: Text("No internet"), message: Text("You have no internet connection."), dismissButton: .cancel(Text("Cancel")))
+        })
+    }
+    
+    func placeOrder() {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encoded
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                showingConnectionError = true
+                print("Error case")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error").")
+                return
+            }
+            
+            if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+                self.confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+                self.showingConfirmation = true
+            } else {
+                print("Invalid response from server")
+            }
+        }.resume()
     }
 }
 
 struct CheckoutView_Previews: PreviewProvider {
     static var previews: some View {
-        CheckoutView()
+        CheckoutView(order: Order())
     }
 }
