c11ca5a7615f49261cc0b48462079fdc0dba938b
crabucate <info@crabucate.de>
2021-01-31 17:24:21 +0100

    Add Bookworm Project 11

--- 15-Bookworm (Project 11)/Bookworm/EmojiRatingView.swift
+++ 15-Bookworm (Project 11)/Bookworm/EmojiRatingView.swift
@@ -8,13 +8,26 @@
 import SwiftUI
 
 struct EmojiRatingView: View {
+    let rating: Int16
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        switch rating {
+        case 1:
+            return Text("🐶")
+        case 2:
+            return Text("🐱")
+        case 3:
+            return Text("🐭")
+        case 4:
+            return Text("🐹")
+        default:
+            return Text("🐰")
+        }
     }
 }
 
 struct EmojiRatingView_Previews: PreviewProvider {
     static var previews: some View {
-        EmojiRatingView()
+        EmojiRatingView(rating: 3)
     }
 }
